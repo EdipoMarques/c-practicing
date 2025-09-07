edc602ec5da64ec505a0479ec8fccbae0bc5fc2b
Édipo Marques <edipomarquesl@gmail.com>
2025-09-08 01:35:44 +0200

    Ex01

--- Excercises/Ex01.c
+++ Excercises/Ex01.c
@@ -13,9 +13,12 @@ int main() {
     printf("My first name starts with the letter %c.\n", name); // %c for single characters
     printf("My name is %s.\n", full_name); // %s for strings
 
-    int x = 4;
-    int y = 6;
+    int x = 4; // Integer
+    int y = 6; // Integer
+    int z = 4.45; // Float assigned to an integer variable, the decimal part will be truncated
+
     printf("The sum of %d and %d is %d.\n", x, y, x + y);
+    printf("The division of %d by %d is %d.\n", y, x, y / x); // Integer division
     
     return 0;
 }
