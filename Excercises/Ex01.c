ee8fe838f38addf08b2b5d105dfa8bc5ad7ed51d
Édipo Marques <edipomarquesl@gmail.com>
2025-09-07 19:30:19 +0200

    Ex01

--- Excercises/Ex01.c
+++ Excercises/Ex01.c
@@ -4,12 +4,13 @@ int main() {
 
     int age = 32;
     float height = 1.89;
-    char name = 'Edipo'; // Character, singles quotes and no []
+    char name = 'E'; // Signle Character, singles quotes and no []
     char full_name[] = "Édipo Marques de Lima"; // String (array of characters), thats why we use double quotes and []
 
     printf("I am %d years old.\n", age);
     printf("I am %.1f meters tall.\n", height);
     printf("I am %d years old and %.1f meters tall.\n", age, height);
+    printf("My first name starts with the letter %c.\n", name); // %c for single characters
     printf("My name is %s.\n", full_name); // %s for strings
     
     return 0;
