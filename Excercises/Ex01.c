89997e014fbfdb7fb1cf0f810220918f74108cc7
Édipo Marques <edipomarquesl@gmail.com>
2025-09-12 23:41:29 +0200

    Ex01

--- Excercises/Ex01.c
+++ Excercises/Ex01.c
@@ -49,7 +49,7 @@ int main() {
     
     int age = 0;
     float height = 0;
-    printf("Enter age: ");
+    printf("Enter your age: ");
     scanf("%d", &age); // & is the address operator, it gives the memory address of the variable
     printf("You are %d years old.\n", age);
     printf("Enter your height: ");
